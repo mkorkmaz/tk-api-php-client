9b8a858a587793cc0cb7ffcf82557c72172b4544
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2018-10-17 12:02:42 +0300

    Helper class DurationConverter added to calculate duration

---
+++ src/SDK/Helper/DurationConverter.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace TK\SDK\Helper;
+
+/**
+ * Class DurationConverter
+ * @package TK\SDK\Helper
+ *
+ * Converts ISO 8601 formatted duration
+ */
+class DurationConverter
+{
+    public static function toSeconds(string $duration) : float
+    {
+        preg_match('/^P(.*?)Y(.*?)M(.*?)DT(.*?)H(.*?)M(.*?)S$/', $duration, $matches);
+
+        list ($ignore, $year, $month, $day, $hour, $minute, $second) = array_map(function($item) { return (float) $item;}, $matches);
+
+        $seconds = $year * 365 * 24 * 60 * 60
+            + $month * 30 * 24 * 60 * 60
+            + $day * 24 * 60 * 60
+            + $hour * 60 * 60
+            + $minute * 60
+            + $second
+        ;
+        return $seconds;
+    }
+
+    public static function toMinute(string $duration) : float
+    {
+        return self::toSeconds($duration) / 60;
+    }
+    public static function toHour(string $duration) : float
+    {
+        return self::toMinute($duration) / 60;
+    }
+
+    public static function toDay(string $duration) : float
+    {
+        return self::toHour($duration) / 24;
+    }
+
+    public static function toMonth(string $duration) : float
+    {
+        return self::toDay($duration) / 30;
+    }
+
+    public static function toYear(string $duration) : float
+    {
+        return round(self::toDay($duration) / 365, 2);
+    }
+
+
+}
---
+++ tests/unit/Helper/DurationConverterTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace TK\Test\Unit\Helper;
+
+use TK\SDK\Helper\DurationConverter;
+
+class DurationConverterTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    /**
+     * @test
+     */
+    public function shouldConvertDurationToSecondsSuccessfully() : void
+    {
+        $duration = 'P0Y0M0DT1H5M0S';
+        $expected = 1*60*60 + 5*60 + 0;
+        $result = DurationConverter::toSeconds($duration);
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldConvertDurationToYearSuccessfully() : void
+    {
+        $duration = 'P1Y9M4DT1H5M0S';
+        $expected = round(1 + 9*30/365 + 4/365 + 1/365/60 + 5/365/60/60, 2);
+        $result = DurationConverter::toYear($duration);
+        $this->assertEquals($expected, $result);
+    }
+}
