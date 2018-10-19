801f22e48b06cbcf4da00b2aaa1efc32b7c52138
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2018-10-20 01:57:55 +0300

    GetAvailabilityParametersFactory added

---
+++ tests/unit/Factory/GetAvailabilityParametersFactoryTest.php
@@ -0,0 +1,101 @@
+<?php
+declare(strict_types=1);
+
+namespace TK\Test\Unit\Factory;
+
+use TK\SDK\ValueObject\Factory\GetAvailabilityParametersFactory;
+use TK\SDK\ValueObject\GetAvailabilityParameters;
+class GetAvailabilityParametersFactoryTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    /**
+     * @test
+     * @throws \Exception
+     */
+    public function shouldReturnGetAvailabilityParameters() : void
+    {
+        $json =<<<JSON
+{
+  "ReducedDataIndicator":false,
+  "RoutingType":"r",
+  "PassengerTypeQuantity":[
+    {
+      "Code":"adult",
+      "Quantity":1
+    },
+    {
+      "Code":"child",
+      "Quantity":1
+    },
+    {
+      "Code":"infant",
+      "Quantity":0
+    }
+  ],
+  "OriginDestinationInformation":[
+    {
+      "DepartureDateTime":{
+        "WindowAfter":"P0D",
+        "WindowBefore":"P0D",
+        "Date":"14OCT"
+      },
+      "OriginLocation":{
+        "LocationCode":"IST",
+        "MultiAirportCityInd":true
+      },
+      "DestinationLocation":{
+        "LocationCode":"ESB",
+        "MultiAirportCityInd":true
+      },
+      "CabinPreferences":[
+        {
+          "Cabin":"ECONOMY"
+        },
+        {
+          "Cabin":"BUSINESS"
+        }
+      ]
+    },
+    {
+      "DepartureDateTime":{
+        "WindowAfter":"P0D",
+        "WindowBefore":"P0D",
+        "Date":"09JAN"
+      },
+      "OriginLocation":{
+        "LocationCode":"ESB",
+        "MultiAirportCityInd":false
+      },
+      "DestinationLocation":{
+        "LocationCode":"IST",
+        "MultiAirportCityInd":false
+      },
+      "CabinPreferences":[
+        {
+          "Cabin":"ECONOMY"
+        },
+        {
+          "Cabin":"BUSINESS"
+        }
+      ]
+    }
+  ]
+}
+JSON;
+        $parameterObject = GetAvailabilityParametersFactory::createFromJson($json);
+        $this->assertInstanceOf(GetAvailabilityParameters::class, $parameterObject);
+        $this->assertEquals(json_decode($json, true), $parameterObject->getValue());
+    }
+}
