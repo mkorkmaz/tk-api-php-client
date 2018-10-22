5ea523144a6164dfd696d90cfed2fc932d078e67
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2018-10-22 15:46:30 +0300

    DurationConverterTest coverage increased

--- tests/unit/Helper/DurationConverterTest.php
+++ tests/unit/Helper/DurationConverterTest.php
@@ -39,4 +39,15 @@ class DurationConverterTest extends \Codeception\Test\Unit
         $result = DurationConverter::toYear($duration);
         $this->assertEquals($expected, $result);
     }
+
+    /**
+     * @test
+     */
+    public function shouldConvertDurationToMonthSuccessfully() : void
+    {
+        $duration = 'P0Y9M4DT1H5M0S';
+        $expected = round( 9*30/365 + 4/365 + 1/365/60 + 5/365/60/60, 2);
+        $result = DurationConverter::toYear($duration);
+        $this->assertEquals($expected, $result);
+    }
 }
