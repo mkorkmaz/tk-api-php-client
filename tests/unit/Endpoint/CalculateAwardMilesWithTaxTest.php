2bd8cd1fd96da73a31072133b386bc9734c6bcc8
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2018-10-23 14:08:45 +0300

    more tests

--- tests/unit/Endpoint/CalculateAwardMilesWithTaxTest.php
+++ tests/unit/Endpoint/CalculateAwardMilesWithTaxTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace TK\Test\Unit\Endpoint;
 
 use DateTimeImmutable;
+use TK\SDK\Exception\InvalidArgumentException;
 use TK\SDK\ValueObject\CalculateAwardMilesWithTaxParameters;
 
 class CalculateAwardMilesWithTaxTest extends EndpointAbstract
@@ -21,6 +22,29 @@ class CalculateAwardMilesWithTaxTest extends EndpointAbstract
             ->withDepartureOrigin('IST')
             ->withDepartureDestination('JFK')
             ->withDepartureDate(new DateTimeImmutable($departureDate));
+
+        $response = $this->client->calculateAwardMilesWithTax($calculateAwardMilesWithTaxParameters);
+        $this->assertEquals(200, $response['status']);
+        $this->assertEquals('SUCCESS', $response['response']['status']);
+        $this->assertEquals('TK-0000', $response['response']['code']);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetResponseSuccessfullyForDifferentInputs() : void
+    {
+        $arrivalDate = gmdate('Y-m-d H:i:s', strtotime('+4 days'));
+
+        $calculateAwardMilesWithTaxParameters = (new CalculateAwardMilesWithTaxParameters(
+            CalculateAwardMilesWithTaxParameters::AWARD_TYPE_ECONOMY
+        ))->withOneWay()
+            ->withSeatGuaranteed()
+            ->withPassengerType('FFY')
+            ->withArrivalOrigin('IST')
+            ->withArrivalDestination('JFK')
+            ->withArrivalDate(new DateTimeImmutable($arrivalDate));
+
         $response = $this->client->calculateAwardMilesWithTax($calculateAwardMilesWithTaxParameters);
         $this->assertEquals(200, $response['status']);
         $this->assertEquals('SUCCESS', $response['response']['status']);
--- tests/unit/Factory/CalculateAwardMilesWithTaxParametersFactoryTest.php
+++ tests/unit/Factory/CalculateAwardMilesWithTaxParametersFactoryTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace TK\Test\Unit\Factory;
 
+use TK\SDK\Exception\InvalidArgumentException;
 use TK\SDK\ValueObject\Factory\CalculateAwardMilesWithTaxParametersFactory;
 use TK\SDK\ValueObject\CalculateAwardMilesWithTaxParameters;
 
@@ -26,7 +27,7 @@ class CalculateAwardMilesWithTaxParametersFactoryTest extends \Codeception\Test\
      * @test
      * @throws \Exception
      */
-    public function shouldReturnCalculateAwardMilesWithTaxParameters() : void
+    public function shouldReturnCalculateAwardMilesWithTaxParametersObjectSuccessfully() : void
     {
         $json =<<<JSON
 {
@@ -43,5 +44,47 @@ JSON;
         $parameterObject = CalculateAwardMilesWithTaxParametersFactory::createFromJson($json);
         $this->assertInstanceOf(CalculateAwardMilesWithTaxParameters::class, $parameterObject);
         $this->assertEquals(json_decode($json, true), $parameterObject->getValue());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldFailForInvalidAwardTypeSuccessfully() : void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $json =<<<JSON
+{
+    "awardType": "Z",
+    "wantMoreMiles": "T",
+    "isOneWay": "T",
+    "departureOrigin": "IST",
+    "departureDestination": "FRA",
+    "departureDateDay": 12,
+    "departureDateMonth": 11,
+    "departureDateYear": 2017
+}
+JSON;
+        $parameterObject = CalculateAwardMilesWithTaxParametersFactory::createFromJson($json);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldFailForInvalidIataCodeSuccessfully() : void
+    {
+        //$this->expectException(InvalidArgumentException::class);
+        $json =<<<JSON
+{
+    "awardType": "E",
+    "wantMoreMiles": "T",
+    "isOneWay": "T",
+    "departureOrigin": "ICAO",
+    "departureDestination": "FRA",
+    "departureDateDay": 12,
+    "departureDateMonth": 11,
+    "departureDateYear": 2017
+}
+JSON;
+        $parameterObject = CalculateAwardMilesWithTaxParametersFactory::createFromJson($json);
     }
 }
